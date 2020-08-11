ba98f7809889878f38c10b8e371c60ba63372bdd
Sam Debruyn <sam@debruyn.dev>
2020-08-12 01:38:35 +0200

    fix attr

--- terraform/scripts.tf
+++ terraform/scripts.tf
@@ -4,6 +4,6 @@ resource "azurerm_storage_blob" "download_install_python" {
   storage_container_name = azurerm_storage_container.scripts.name
   type                   = "Block"
   source_content = templatefile("${path.module}/../scripts/download_install_python.sh", {
-    download_url = "${azurerm_storage_blob.sensors.url}${data.azurerm_storage_account_blob_container_sas.python_sas}"
+    download_url = "${azurerm_storage_blob.sensors.url}${data.azurerm_storage_account_blob_container_sas.python_sas.sas}"
   })
 }
