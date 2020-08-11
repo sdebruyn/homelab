1167fce45565badf768076e86cf0a085eef50ab9
Sam Debruyn <sam@debruyn.dev>
2020-08-12 00:38:16 +0200

    add github workflow

--- terraform/python.tf
+++ terraform/python.tf
@@ -1,12 +1,3 @@
-resource "null_resource" "package" {
-  provisioner "local-exec" {
-    working_dir = "${path.module}/../python/"
-    command     = "python3 setup.py sdist"
-  }
-}
-
 data "local_file" "package" {
-  depends_on = [
-  null_resource.package]
   filename = "${path.module}/../python/dist/sensors-0.1.tar.gz"
 }
