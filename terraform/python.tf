c1fda09f237fe4954db226d4c0cf8f8f71e93b66
Sam Debruyn <sam@debruyn.dev>
2020-08-26 20:50:22 +0200

    download and install directly

--- terraform/python.tf
+++ terraform/python.tf
@@ -4,4 +4,11 @@ resource "azurerm_storage_blob" "sensors" {
   storage_container_name = azurerm_storage_container.python.name
   type                   = "Block"
   source                 = "${path.module}/../python/dist/sensors-0.1.tar.gz"
+
+  provisioner "local-exec" {
+    command = "curl -X POST -sl \"https://${local.server}/homelab-trigger-deployment\" -H \"Content-Type: application/json\" -d \"{\\\"secret\\\": \\\"$secret\\\"}\""
+    environment = {
+      secret = data.azurerm_key_vault_secret.deployment_secret.value
+    }
+  }
 }
