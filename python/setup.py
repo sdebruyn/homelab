9a5c8504e6bf3573433f002061f6fb6d74169ac9
Sam Debruyn <sam@debruyn.dev>
2020-08-26 21:05:23 +0200

    use simple script instead of package

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="sensors",
     version="0.1",
-    packages=find_packages(),
+    py_modules=['sensors'],
     entry_points={
         'console_scripts': ['send-measurement-azure=sensors:send_measurements'],
     },
