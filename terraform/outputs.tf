331973d26dc4e2ff9546ac882cf3735bc46ab75f
Sam Debruyn <sam@debruyn.dev>
2020-08-26 21:57:09 +0200

    send event hub params to rpi

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,3 +11,12 @@ output "deployment_secret" {
   value     = data.azurerm_key_vault_secret.deployment_secret.value
   sensitive = true
 }
+
+output "eventhub_name" {
+  value = azurerm_eventhub.hub.name
+}
+
+output "eventhub_connection_string" {
+  value     = azurerm_eventhub_namespace.ehns.default_primary_connection_string
+  sensitive = true
+}
