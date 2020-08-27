703961adfcd0a4d5af5f4f2cb3faab145b4c1d0f
Sam Debruyn <sam@debruyn.dev>
2020-08-27 21:27:04 +0200

    lowercase sku name

--- terraform/databricks.tf
+++ terraform/databricks.tf
@@ -2,7 +2,7 @@ resource "azurerm_databricks_workspace" "dbks" {
   location                    = local.region
   name                        = "dbks${local.name}"
   resource_group_name         = azurerm_resource_group.rg.name
-  sku                         = "Standard"
+  sku                         = "standard"
   managed_resource_group_name = "rgdbks${local.name}"
   tags                        = local.tags
 }
