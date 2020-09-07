4323975f058a15c433f1e58bd2bcef2f80fc29bd
Sam Debruyn <sam@debruyn.dev>
2020-09-08 00:12:05 +0200

    cleanup helm failure

--- terraform/spark.tf
+++ terraform/spark.tf
@@ -5,8 +5,11 @@ resource "kubernetes_namespace" "spark" {
 }
 
 resource "helm_release" "spark_op" {
-  repository = "http://storage.googleapis.com/kubernetes-charts-incubator"
-  chart      = "sparkoperator"
-  name       = "sparkoperator"
-  namespace  = kubernetes_namespace.spark.metadata[0].name
+  repository      = "http://storage.googleapis.com/kubernetes-charts-incubator"
+  chart           = "sparkoperator"
+  name            = "sparkoperator"
+  namespace       = kubernetes_namespace.spark.metadata[0].name
+  cleanup_on_fail = true
+  atomic          = true
+  wait            = true
 }
