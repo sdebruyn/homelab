59ba6fdbec92aa0a662fbec5143f77250e455af7
Sam Debruyn <sam@debruyn.dev>
2020-09-07 22:54:32 +0200

    remove old sas

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -11,21 +11,3 @@ resource "azurerm_storage_container" "python" {
   name                 = "python"
   storage_account_name = azurerm_storage_account.sa.name
 }
-
-data "azurerm_storage_account_blob_container_sas" "python_sas" {
-  depends_on = [
-  azurerm_storage_container.python]
-  connection_string = azurerm_storage_account.sa.primary_blob_connection_string
-  container_name    = azurerm_storage_container.python.name
-  expiry            = timeadd(timestamp(), "87600h")
-  start             = timestamp()
-
-  permissions {
-    add    = false
-    create = false
-    delete = false
-    list   = false
-    read   = true
-    write  = false
-  }
-}
