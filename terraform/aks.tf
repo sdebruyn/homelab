c4bf3cd7691dd5e98bbc6f5674bf99cb2ecb12a1
Sam Debruyn <sam@debruyn.dev>
2020-09-07 23:52:49 +0200

    k8s and helm config

---
+++ terraform/aks.tf
@@ -0,0 +1,59 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks${local.name}"
+  location            = local.region
+  tags                = local.tags
+  resource_group_name = azurerm_resource_group.rg.name
+  node_resource_group = "rgaks${local.name}"
+  dns_prefix          = local.name
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name       = "default"
+    vm_size    = "Standard_D3_v2"
+    node_count = 2
+  }
+
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.la.id
+    }
+  }
+}
+
+resource "azurerm_log_analytics_solution" "k8s" {
+  solution_name         = "ContainerInsights"
+  location              = local.region
+  resource_group_name   = azurerm_resource_group.rg.name
+  workspace_resource_id = azurerm_log_analytics_workspace.la.id
+  workspace_name        = azurerm_log_analytics_workspace.la.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}
+
+provider "kubernetes" {
+  load_config_file       = false
+  host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  username               = azurerm_kubernetes_cluster.aks.kube_config.0.username
+  password               = azurerm_kubernetes_cluster.aks.kube_config.0.password
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+    username               = azurerm_kubernetes_cluster.aks.kube_config.0.username
+    password               = azurerm_kubernetes_cluster.aks.kube_config.0.password
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+  }
+}
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -6,5 +6,11 @@ terraform {
     random = {
       source = "hashicorp/random"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    helm = {
+      source = "hashicorp/helm"
+    }
   }
 }
